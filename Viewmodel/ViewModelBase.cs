e961c100c5aa7939fa3a7d0a0949da05433ef3c5
ChristianFranik <christian.franik@t-online.de>
2020-01-23 23:16:19 +0100

    Notifier class

--- Viewmodel/ViewModelBase.cs
+++ Viewmodel/ViewModelBase.cs
@@ -1,51 +1,12 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-namespace Wpf.Viewmodel
+﻿namespace Wpf.Viewmodel
 {
     /// <summary>
     /// Base class for view models
     /// Implements System.ComponentModel.INotifyPropertyChanged for easy
     /// changing of properties
     /// </summary>
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public abstract class ViewModelBase : Notifier
     {
-        //======================================================================
-        // Public methods
-        //======================================================================
-        /// <summary>
-        /// Event handler for property changes
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        //======================================================================
-        // Protected methods
-        //======================================================================
-        /// <summary>
-        /// Invoker for property change event
-        /// </summary>
-        /// <param name="propertyName">Name of the changed property. Gets supplied by runtime</param>
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        /// <summary>
-        /// Template method to set properties in inheriting classes
-        /// </summary>
-        /// <typeparam name="T">property type</typeparam>
-        /// <param name="storage">(Private) variable that stores the property value</param>
-        /// <param name="value">(New) value to set</param>
-        /// <param name="propertyName">Name of the changed property. Gets supplied by runtime</param>
-        /// <returns></returns>
-        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
-        {
-            if (EqualityComparer<T>.Default.Equals(storage, value))
-                return false;
-            storage = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
     }
 }
